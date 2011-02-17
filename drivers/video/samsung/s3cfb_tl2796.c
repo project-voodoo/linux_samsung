c15177c608f895bb29e93d5732b984bc48ffd2a6
SIMOND François <francois@lienweb.fr>
2011-02-17 12:44:59 +0100

    Voodoo color: minor refactoring by code police^^

--- drivers/video/samsung/s3cfb_tl2796.c
+++ drivers/video/samsung/s3cfb_tl2796.c
@@ -319,12 +319,13 @@ static ssize_t gamma_table_show(struct device *dev, struct device_attribute *att
 	for(point = 0; point < lcd_->data->gamma_table_size; point++)
 	{
 		for(color = 0; color < 3; color++)
-			if (color == 2)
-				sprintf(buf,"%s%u", buf, lcd_->data->gamma_table[point].v[color] );
-			else
-				sprintf(buf,"%s%u,", buf, lcd_->data->gamma_table[point].v[color] );
+		{
+			sprintf(buf,"%s%u", buf, lcd_->data->gamma_table[point].v[color] );
+			if (color != 2)
+				sprintf(buf,"%s,", buf);
+		}
 
-		sprintf(buf,"%s\n", buf );
+		sprintf(buf,"%s\n", buf);
 	}
 
 	return sprintf(buf, "%s", buf);
